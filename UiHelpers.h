3278186a386d73486e983de0de6bd0a23b7320e5
Ivo van Poorten <ivopvp@gmail.com>
2025-01-20 06:13:45 +0100

    Use fltk.h in UiHelpers

--- UiHelpers.h
+++ UiHelpers.h
@@ -1,14 +1,4 @@
-#include <FL/Fl.H>
-#include <FL/Fl_Double_Window.H>
-#include <FL/Fl_Box.H>
-#include <FL/Fl_Button.H>
-#include <FL/Fl_Radio_Button.H>
-#include <FL/Fl_Choice.H>
-#include <FL/Fl_Group.H>
-#include <FL/Fl_Hor_Value_Slider.H>
-#include <FL/Fl_Pack.H>
-#include <FL/Fl_Select_Browser.H>
-#include <FL/Fl_Table.H>
+#include "fltk.h"
 
 class Label : public Fl_Box {
 public:
