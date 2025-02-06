d2411c319c7527e6d054b828582938341e118453
Ivo van Poorten <ivopvp@gmail.com>
2025-02-06 04:13:43 +0100

    Implement Windows reparenting of HWND. This works correctly in Reaper, but not in Carla. Mouse events work as expected, but Carla inhibits almost all keyboard events. So use Reaper, not Carla ;)

--- src/platform.cpp
+++ src/platform.cpp
@@ -25,8 +25,40 @@ void ReparentWindow(Window w, Window parentWindow) {
 }
 
 #elif _WIN32
-    // windows code goes here, reparent HWND
-    #error "WIN32 support not implemented yet"
+
+unsigned int GetWindowUIState(HWND hwnd) {
+    return LOWORD(SendMessage(hwnd, WM_QUERYUISTATE, 0, 0));
+}
+
+void ReparentWindow(Window w, Window parentWindow) {
+    // Note that Carla still misses around 90% of the keyboard events. This
+    // seems to be a bug in Carla, as Reaper works 100% correctly.
+    //
+    // Change Window Style, setting WS_CHILD blocks all keyboard and mouse
+    // events, so we don't set that flag, but we do remove the window's caption
+    //
+    unsigned int style = GetWindowLong((HWND) w, GWL_STYLE);
+    //style = (style | WS_CHILD) & (~WS_POPUP) & (~WS_CAPTION);
+    style = (style) & (~WS_POPUP) & (~WS_CAPTION);
+    SetWindowLong((HWND) w, GWL_STYLE, style);
+
+    // Reposition at (0,0)
+    SetWindowPos((HWND) w, nullptr, 0, 0, 123, 123, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+    SetParent((HWND) w, (HWND) parentWindow);
+
+    // Synchronize UI States
+    auto parentUIState = GetWindowUIState((HWND) parentWindow);
+    auto currentState = GetWindowUIState((HWND) w);
+    auto missingState = parentUIState & ~currentState;
+    if (missingState) {
+        SendMessage((HWND) w, WM_UPDATEUISTATE, MAKEWPARAM(UIS_SET, missingState), 0);
+    }
+    auto extraState = currentState & ~parentUIState;
+    if (extraState) {
+        SendMessage((HWND) w, WM_UPDATEUISTATE, MAKEWPARAM(UIS_CLEAR, extraState), 0);
+    }
+}
+
 #elif __APPLE__
     // macOS code goes here, reparent NSView
     #error "macOS support not implemented yet"
